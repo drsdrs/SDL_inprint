765e1875f24eed9b097699711a3d23a8243f16f7
drsdrs <eidenberger_andreas@gmx.at>
2023-06-06 17:32:40 +0200

    changes where made

--- SDL2_inprint.h
+++ SDL2_inprint.h
@@ -3,16 +3,16 @@
 
 #include <SDL2/SDL.h>
 
-extern void prepare_inline_font(void);
-extern void kill_inline_font(void);
+extern void prepare_inline_font( void );
+extern void kill_inline_font( void );
 
-extern void inrenderer(SDL_Renderer *renderer);
-extern void infont(SDL_Texture *font);
-extern void incolor1(SDL_Color *color);
-extern void incolor(Uint32 color, Uint32 unused); /* Color must be in 0x00RRGGBB format ! */
-extern void inprint(SDL_Renderer *dst, const char *str, Uint32 x, Uint32 y);
-extern void inprint_char(SDL_Renderer *dst, const char ch, Uint32 x, Uint32 y);
+extern void inrenderer( SDL_Renderer *renderer );
+extern void infont( SDL_Texture *font );
+extern void incolor1( SDL_Color *color );
+extern void incolor( Uint32 color, Uint32 unused ); /* Color must be in 0x00RRGGBB format ! */
+extern void inprint( SDL_Renderer *dst, const char *str, Uint32 x, Uint32 y );
+extern void inprint_char( SDL_Renderer *dst, const char ch, Uint32 x, Uint32 y );
 
-extern SDL_Texture *get_inline_font(void);
+extern SDL_Texture *get_inline_font( void );
 
 #endif /* SDL2_inprint_h */
